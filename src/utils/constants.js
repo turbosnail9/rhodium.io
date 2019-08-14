a5d1e17a2afb2a5bf7eccfd9137f8279561dc707
Ari Ramdial <ari@kn0x.io>
2019-08-13 21:52:19 -0400

    updates content

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,18 +2,18 @@ export const LAMBDA_ENDPOINT = 'https://kvz60gz535.execute-api.us-east-1.amazona
 export const IS_PROD = process.env.NODE_ENV === 'production'
 export const META = {
   common: {
-    image: '/images/michele-mazzucco-meta-image.jpg',
+    image: '/images/profile.jpg',
   },
   index: {
-    title: 'Michele Mazzucco | Interdisciplinary designer',
-    description: 'Michele Mazzucco, interdisciplinary designer based in Brescia, Italy. I focus primarily on building user interfaces that are usable and scalable.'
+    title: 'Rhodium Labs | Heretics',
+    description: 'Rhodium Labs, heretical venture studio based in Toronto, Canada. We build products that make the world a bit more sane.'
   },
-  profile: {
-    title: 'Michele Mazzucco | Profile',
-    description: 'Over the last 4+ years, I have been working with small- and medium-size companies all over the world.'
+  about: {
+    title: 'Rhodium Labs | About',
+    description: 'Over the last 4+ years, we have worked with the world\'s best companies.'
   },
   works: {
-    title: 'Michele Mazzucco | Works',
+    title: 'Rhodium Labs | Works',
     description: 'A selection of the work carried out over 4+ years of work. Among which: Websites, Web applications, coordinated images and more.'
   }
 }
